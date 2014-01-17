2fb462291b83e3cc5934b6def7e3736e38b408e4
butschster <butschster@gmail.com>
2014-01-17 11:43:54 +0400

    init

---
+++ init.php
@@ -0,0 +1,6 @@
+<?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
+
+Plugin::factory('yandexmetrika', array(
+	'title' => 'Yandex Метрика',
+	'description' => 'Метрика посещения пользователями сайта',
+))->register();
